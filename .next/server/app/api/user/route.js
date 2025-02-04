da93c3f1a230d93d46be1d3d7b617bbdbb28474e
Sah!l <sahil.maji@gmail.com>
2025-02-04 11:56:40 +0530

    Untrack files in .gitignore

--- .next/server/app/api/user/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/user/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_27df43._.js");
-runtime.loadChunk("server/chunks/[root of the server]__ea3580._.js");
-runtime.loadChunk("server/chunks/_3bc0a3._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/user/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/user/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6sn2n2B8JPwkXohplcK3Bvd1ELBAULGWUXsHPQg8O60=\"\n}"
