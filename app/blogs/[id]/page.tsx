3d544885f8783e4fe79b1c660cdc41be3323be29
sahil <sahil.maji@gmail.com>
2025-04-16 02:58:34 +0530

    remove unnecessary function

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -15,21 +15,21 @@ export async function generateStaticParams() {
 }
 
 // Generate metadata for the page
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const post = await getNewsById(params.id);
+// export async function generateMetadata({ params }: { params: { id: string } }) {
+//   const post = await getNewsById(params.id);
   
-  if (!post) {
-    return {
-      title: 'Article Not Found - Vigilance360',
-      description: 'The requested article could not be found.',
-    };
-  }
+//   if (!post) {
+//     return {
+//       title: 'Article Not Found - Vigilance360',
+//       description: 'The requested article could not be found.',
+//     };
+//   }
   
-  return {
-    title: `${post.title} - Vigilance360`,
-    description: post.description,
-  };
-}
+//   return {
+//     title: `${post.title} - Vigilance360`,
+//     description: post.description,
+//   };
+// }
 
 async function BlogPostContent({ id }: { id: string }) {
   const post = await getNewsById(id);
